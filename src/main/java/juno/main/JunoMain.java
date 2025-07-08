1788cdae6af1eb9c86898b7c0006a33db46ea0e5
Abdullah Omar Nasif <25197012+naasif08@users.noreply.github.com>
2025-07-08 20:21:57 +0600

    Initial Commit

--- src/main/java/juno/main/JunoMain.java
+++ src/main/java/juno/main/JunoMain.java
@@ -1,8 +1,8 @@
-package juno;
+package juno.main;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class JunoMain {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
